88befc75ba38f2565cb8a49806a7fb85ee925a22
manoj <manojkumarmsd9204@gmail.com>
2021-03-23 17:56:39 +0530

    layout stream

--- lib/Stream.dart
+++ lib/Stream.dart
@@ -13,7 +13,7 @@ class _StreamState extends State<Stream> {
       body: Container(
         /* bg image */
         height: 850,
-        width: 500,
+        width: 600,
         decoration: BoxDecoration(
           image: DecorationImage(
             image: NetworkImage("http://via.placeholder.com/300"),
@@ -44,7 +44,7 @@ class _StreamState extends State<Stream> {
                 ),
             ),
             Positioned(
-              top: 49,
+              top: 52,
               left: 60,
               child: Container(
                 child: Icon(Icons.remove_red_eye_outlined,size: 13.0),
@@ -98,6 +98,127 @@ class _StreamState extends State<Stream> {
                 ),
               ),
             ),
+            Positioned(
+                top: 305,
+                left: 15,
+                child: Container(
+                  width: 45.0,
+                  height: 45.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover, image: NetworkImage('https://images.unsplash.com/photo-1597466765990-64ad1c35dafc')),
+                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  ),
+                )
+            ),
+            Positioned(
+                top: 352,
+                left: 18,
+                child: Container(
+                    child: Text(
+                      '10 Left',
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal, fontSize: 13
+                      ),)
+                )
+            ),
+            Positioned(
+                top: 395,
+                left: 18,
+                child: Container(
+                    child: CircleAvatar(
+                      radius: 14,
+                      backgroundImage: NetworkImage( "https://images.unsplash.com/photo-1597466765990-64ad1c35dafc"),
+                    )
+                )
+            ),
+            Positioned(
+                top: 395,
+                left: 55,
+                child: Container(
+                    child: Text(
+                      'John Williams',
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal, fontSize: 12
+                      ),)
+                )
+            ),
+            Positioned(
+                top: 410,
+                left: 55,
+                child: Container(
+                    child: Text(
+                      'Good Product',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 13
+                      ),)
+                )
+            ),
+            Positioned(
+                top: 440,
+                left: 18,
+                child: Container(
+                    child: CircleAvatar(
+                      radius: 14,
+                      backgroundImage: NetworkImage( "https://images.unsplash.com/photo-1597466765990-64ad1c35dafc"),
+                    )
+                )
+            ),
+            Positioned(
+                top: 440,
+                left: 55,
+                child: Container(
+                    child: Text(
+                      'John Williams',
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal, fontSize: 12
+                      ),)
+                )
+            ),
+            Positioned(
+                top: 455,
+                left: 55,
+                child: Container(
+                    child: Text(
+                      'Good Product',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 13
+                      ),)
+                )
+            ),
+            Positioned(
+                top: 485,
+                left: 18,
+                child: Container(
+                    child: CircleAvatar(
+                      radius: 14,
+                      backgroundImage: NetworkImage( "https://images.unsplash.com/photo-1597466765990-64ad1c35dafc"),
+                    )
+                )
+            ),
+            Positioned(
+                top: 485,
+                left: 55,
+                child: Container(
+                    child: Text(
+                      'John Williams',
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal, fontSize: 12
+                      ),)
+                )
+            ),
+            Positioned(
+                top: 500,
+                left: 55,
+                child: Container(
+                    child: Text(
+                      'Good Product',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 13
+                      ),)
+                )
+            ),
+
           ],
         ),
       ),
