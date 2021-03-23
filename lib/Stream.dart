160728bc34b30886e2b3346a1279c7b05c8faaae
manoj <manojkumarmsd9204@gmail.com>
2021-03-23 16:54:25 +0530

    first row

--- lib/Stream.dart
+++ lib/Stream.dart
@@ -9,139 +9,98 @@ class _StreamState extends State<Stream> {
 
   @override
   Widget build(BuildContext context) {
-    const IconData power_settings_new = IconData(0xe949, fontFamily: 'MaterialIcons');
     return Scaffold(
-      body : Container(
-        padding: EdgeInsets.all(15),
+      body: Container(
         /* bg image */
+        height: 850,
+        width: 500,
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: NetworkImage("http://via.placeholder.com"),
+            image: NetworkImage("http://via.placeholder.com/300"),
             fit: BoxFit.cover,
           ),
         ),
-        /* first row */
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            /* first column */
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only( top:30),
-                ),
-            CircleAvatar(
-              radius: 18,
-              backgroundImage: NetworkImage( "https://images.unsplash.com/photo-1597466765990-64ad1c35dafc"),
-            )],
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              top: 30,
+              left: 15,
+            child: Container(
+                child: CircleAvatar(
+                  radius: 18,
+                  backgroundImage: NetworkImage( "https://images.unsplash.com/photo-1597466765990-64ad1c35dafc"),
+                )
+              )
             ),
-
-            /* second column with crossaxis */
-
-            Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(top:30,right: 35),
-                  child: Text(
-                    'JaneSmith',
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-
-            /* third column with crossaxis */
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(width: 90.0,
-                    margin: new EdgeInsets.symmetric(vertical: 30),
-                  child: OutlinedButton(
-                    onPressed: () {
-                      // Respond to button press
-                    },
-                    child: Text("Switch Product",
+            Positioned(
+                top: 35,
+                left: 60,
+                child: Container(
+                    child: Text(
+                      'JaneSmith',
                       style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                          fontSize: 10,
-                          color: Colors.blue
-                      ),),
-                  )
+                        fontWeight: FontWeight.normal, fontSize: 12
+                      ),)
                 ),
-              ],
             ),
-
-            /* fourth column with crossaxis */
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(width: 55.0,
-                    margin: new EdgeInsets.symmetric(vertical: 30),
-                    child: OutlinedButton(
-                      onPressed: () {
-                        // Respond to button press
-                      },
-                      child: Row( // Replace with a Row for horizontal icon + text
-                        children: <Widget>[
-                          Icon(power_settings_new,size: 14),
-                          Text("END",
-                            style: TextStyle(
-                                fontWeight: FontWeight.normal,
-                                fontSize: 10,
-                                color: Colors.blue
-                            ),),
-                        ],
-                      ),
-                    )
+            Positioned(
+              top: 49,
+              left: 60,
+              child: Container(
+                child: Icon(Icons.remove_red_eye_outlined,size: 13.0),
+              ),
+            ),
+            Positioned(
+              top: 50,
+              left: 76,
+              child: Container(
+                  child: Text(
+                    '4500',
+                    style: TextStyle(
+                        fontWeight: FontWeight.normal, fontSize: 10
+                    ),)
+              ),
+            ),
+            Positioned(
+              top: 50,
+              left: 102,
+              child: Container(
+                  child: Text(
+                    'viewers',
+                    style: TextStyle(
+                        fontWeight: FontWeight.normal, fontSize: 10
+                    ),)
+              ),
+            ),
+            Positioned(
+              top: 35,
+              left: 200,
+              width: 90,
+              child: Container(
+                  child: RaisedButton(
+                    child: Text(" Switch Product ",   style: TextStyle(
+                        fontWeight: FontWeight.normal, fontSize: 10, color: Colors.black
+                    )),
+                    color: Color(0xffe5dddd),
+                  ),
+              ),
+            ),
+            Positioned(
+              top: 35,
+              left: 300,
+              width: 50,
+              child: Container(
+                child: RaisedButton(
+                  child: Text(" END ",   style: TextStyle(
+                      fontWeight: FontWeight.normal, fontSize: 10, color: Colors.black
+                  )),
+                  color: Color(0xffe5dddd),
                 ),
-              ],
+              ),
             ),
-
           ],
         ),
-
-
-
-
-
-
       ),
     );
   }
-
 }
-
-
-Widget titleSection = Container(
-  padding: const EdgeInsets.all(2),
-  child: Row(
-    children: [
-      Expanded(
-        /*1*/
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            /*2*/
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: Text(
-                'JaneSmith',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Text(
-              'Kandersteg, Switzerland',
-              style: TextStyle(
-                color: Colors.grey[500],
-              ),
-            ),
-          ],
-        ),
-      ),
-    ],
-  ),
-);
